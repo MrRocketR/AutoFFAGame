a3cf745b2b6aa68357dc66bf1047259da9632080
MrRocketR <kuptsovns@gmail.com>
2022-10-02 23:53:45 +0300

    started to coding player logic

---
+++ src/main/java/engine/Player.java
@@ -0,0 +1,28 @@
+package engine;
+
+import logic.Battle;
+import logic.Turns;
+import model.Race;
+
+public class Player implements Turns, AI, Battle {
+    Race race;
+
+
+    public synchronized void play() {
+
+
+    }
+
+    public Race getRace() {
+        return race;
+    }
+
+    public void setRace(Race race) {
+        this.race = race;
+    }
+
+    @Override
+    public void makeTurn() {
+        //write
+    }
+}
--- src/main/java/logic/Turns.java
+++ src/main/java/logic/Turns.java
@@ -1,4 +1,5 @@
 package logic;
 
 public interface Turns {
+    void makeTurn();
 }
